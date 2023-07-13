d0ffd12d37ae24abfbac57ff451e450971857919
NicolineRusbakChr <nicoline.rusbak@gmail.com>
2023-07-13 18:46:19 +0200

    Basic functionality

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,73 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <div>
+      <h3>Select Categories:</h3>
+      <label v-for="category in categories" :key="category">
+        <input type="checkbox" :value="category" v-model="selectedCategories" /> {{ category }}
+      </label>
+    </div>
+    <button @click="pickRandomItem">Pick Random Item</button>
+    <button @click="goBack" :disabled="!previousItem">Go Back</button>
+    <p v-if="selectedItem">Selected Item: {{ selectedItem.name }}</p>
+    <p v-if="selectedCategory">Category: {{ selectedCategory }}</p>
+    <p v-if="!remainingItems.length && usedItems.length > 0">No more items in category</p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<script>
+import { ref, computed } from 'vue';
+import itemsData from '../assets/items.json';
+
+export default {
+  data() {
+    return {
+      selectedItem: null,
+      selectedCategory: null,
+      previousItem: null,
+      items: ref([]),
+      usedItems: [],
+      categories: [],
+      selectedCategories: [],
+    };
+  },
+  mounted() {
+    this.items = ref(itemsData.items);
+    this.categories = Array.from(new Set(itemsData.items.map(item => item.category)));
+  },
+  computed: {
+    remainingItems() {
+      if (this.selectedCategories.length > 0) {
+        return this.items.filter(item => !this.usedItems.includes(item) && this.selectedCategories.includes(item?.category));
+      } else {
+        return this.items.filter(item => !this.usedItems.includes(item));
+      }
+    },
+  },
+  methods: {
+    pickRandomItem() {
+      if (this.usedItems.length === this.items.length) {
+        this.usedItems = [];
+      }
+
+      if (this.remainingItems.length === 0) {
+        this.usedItems = [];
+        this.previousItem = null;
+      }
+
+      const randomIndex = Math.floor(Math.random() * this.remainingItems.length);
+      this.previousItem = this.selectedItem;
+      this.selectedItem = this.remainingItems[randomIndex];
+      this.selectedCategory = this.selectedItem?.category;
+      this.usedItems.push(this.selectedItem);
+    },
+    goBack() {
+      if (this.previousItem) {
+        this.usedItems.pop();
+        this.selectedItem = this.previousItem;
+        this.selectedCategory = this.selectedItem?.category;
+        this.previousItem = null;
+      }
+    },
+  },
+};
+</script>
